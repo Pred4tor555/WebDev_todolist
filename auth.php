99afbea14f8f9bad1f29ae108e84fea41202f72f
Pred4tor555 <artuk200311@mail.ru>
2025-02-02 15:15:48 +0500

    task4

--- auth.php
+++ auth.php
@@ -1,30 +1,35 @@
 <?php
+global $conn;
 session_start(["use_strict_mode" => true]);
+require('dbconnect.php');
 unset($_SESSION['message']);
+if (isset($_POST['login'])){
+    $result = $conn->query("SELECT * FROM users WHERE email = '".$_POST['login']."'");
 
-$valid_user = 'artem';
-$valid_pass = '123456';
-
-if (!empty($_POST['login']) && !empty($_POST['password'])) {
-    if ($_POST['login'] === $valid_user) {
-        if ($_POST['password'] === $valid_pass) {
+    if ($row = $result->fetch())
+    {
+        if (MD5($_POST["password"]) == $row['password']){
             $_SESSION['username'] = $_POST['login'];
             header("Location: index.php");
-            exit();
-        } else {
-            $_SESSION['message'] = 'Вы ввели неправильный пароль!';
+            die();
         }
-    } else {
-        $_SESSION['message'] = 'Вы ввели неправильный логин!';
-    }
-    header("Location: index.php");
-    exit();
-}
+        else {
+            $_SESSION['message'] = 'Вы ввели неправильный пароль!';
+            header("Location: index.php");
+            die();
+        }
 
-if (isset($_GET['logout']) && $_GET['logout'] == 1) {
-    session_unset();
-    $_SESSION['message'] = 'Вы успешно вышли из системы';
-    header("Location: index.php");
-    exit();
+    }
+    else {
+        $_SESSION['message'] = 'Вы ввели неправильный логин!';
+        header("Location: index.php");
+        die();
+    }
+
 }
-?>
+if ($_GET['logout'] == 1){
+    session_unset();
+    $_SESSION['message'] = 'Вы успешно вышли из сиситемы';
+    header("Location: index.php");
+    die();
+}
